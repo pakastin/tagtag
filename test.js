5c369c2b8446e61cc990d42571b64474228001be
Juha Lindstedt <juha@pakastin.fi>
2020-01-15 14:45:12 +0200

    Add test case, fix syntax

--- test.js
+++ test.js
@@ -1,11 +1,12 @@
-const assert = require("assert");
+const assert = require('assert');
 
-describe("tagtag", () => {
-  const tag = require("./dist/tagtag");
+describe('tagtag', () => {
+  const tag = require('./dist/tagtag');
   const testCases = [
-    [() => tag("h1")("Hello world!"), "<h1>Hello world!</h1>"],
-    [() => tag(".hello")("world"), '<div class="hello">world</div>'],
-    [() => tag("#hello.world")("!"), '<div id="hello" class="world">!</div>']
+    [() => tag('h1')('Hello world!'), '<h1>Hello world!</h1>'],
+    [() => tag('.hello')('world'), '<div class="hello">world</div>'],
+    [() => tag('#hello.world')('!'), '<div id="hello" class="world">!</div>'],
+    [() => tag('hello')('<script>alert("evil")</script>'), '<hello>&lt;script&gt;alert(&quot;evil&quot;)&lt;/script&gt;</hello>']
   ];
 
   testCases.forEach(([fn, result]) => {
