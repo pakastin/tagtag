3483b957eec311c2d0879871b69171fe10c182ad
Juha Lindstedt <juha@pakastin.fi>
2020-01-15 10:39:50 +0200

    Add escapeHTML from @akx

--- src/escape-html.js
+++ src/escape-html.js
@@ -1,13 +1,12 @@
-// ht https://github.com/component/escape-html/blob/b42947eefa79efff01b3fe988c4c7e7b051ec8d8/index.js
 const replacements = {
-  '"': "&quot;",
-  "&": "&amp;",
-  "'": "&#39;",
-  "<": "&lt;",
-  ">": "&gt;"
+  '"': '&quot;',
+  '&': '&amp;',
+  "'": '&#39;',
+  '<': '&lt;',
+  '>': '&gt;'
 };
 const escapesRe = /["'&<>]/g;
 
-export default function escapeHTML(h) {
+export default function escapeHTML (h) {
   return h.replace(escapesRe, s => replacements[s]);
 }
